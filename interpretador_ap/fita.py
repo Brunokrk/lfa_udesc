68b1bae874e946feff2862108cc31fa9e481a147
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-09-24 21:13:43 -0300

    .

--- interpretador_ap/fita.py
+++ interpretador_ap/fita.py
@@ -2,3 +2,7 @@ class Word():
     """Palavra á aceitar ou rejeitar"""
     def __init__(self, word):
         self.word = word
+        self.size = len(self.word)
+
+    def __len__(self):
+        return len(self.word)
